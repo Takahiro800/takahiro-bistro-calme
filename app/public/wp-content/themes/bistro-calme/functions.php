51094753b699c9640770edc6fd646d65ddf67049
Takahiro800 <52635809+Takahiro800@users.noreply.github.com>
2020-03-28 20:54:16 +0900

    02-05 タイトルの区切り文字の変更まで

---
+++ app/public/wp-content/themes/bistro-calme/functions.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * <title>タグを出力する
+ */
+add_theme_support( 'title-tag' );
+add_filter( 'document_title_separator', 'my_document_title_separator' );
+function my_document_title_separator($separator){
+  $separator = '|';
+  return $separator;
+}
