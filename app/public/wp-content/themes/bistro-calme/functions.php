752893ffd67f807bdd46e7163b10337f858c6a49
Takahiro800 <52635809+Takahiro800@users.noreply.github.com>
2020-03-29 08:52:03 +0900

    02-05まで完了

--- app/public/wp-content/themes/bistro-calme/functions.php
+++ app/public/wp-content/themes/bistro-calme/functions.php
@@ -8,3 +8,12 @@ function my_document_title_separator($separator){
   $separator = '|';
   return $separator;
 }
+add_filter( 'document_title_parts', 'my_document_title_parts');
+function my_document_title_parts($title){
+  if (is_home()){
+    unset($title['tagline']); //PHPの配列要素の削除
+    $title['tagline'] = 'BISTRO CALMEは、カジュアルなワインバーよりなビストロです。';
+    //要素の代入
+  }
+  return $title;
+}
