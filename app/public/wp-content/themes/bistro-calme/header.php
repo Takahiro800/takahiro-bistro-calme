cf59d4abf340fa42456a344c8750b85a5cc0e3c9
Takahiro800 <52635809+Takahiro800@users.noreply.github.com>
2020-04-01 08:49:30 +0900

    検索フォームの動作

--- app/public/wp-content/themes/bistro-calme/header.php
+++ app/public/wp-content/themes/bistro-calme/header.php
@@ -32,8 +32,8 @@
 				<p><?php bloginfo('description'); ?></p>
 			</div>
 
-			<form class="header_search">
-				<input type="text" placeholder="キーワードを入力">
+			<form action="<?php echo home_url('/'); ?>" method="get"  class="header_search">
+				<input type="text" name="s" value="<?php the_search_query(); ?>"  placeholder="キーワードを入力">
 				<i class="fas fa-search"></i>
 			</form>
 		</div>
